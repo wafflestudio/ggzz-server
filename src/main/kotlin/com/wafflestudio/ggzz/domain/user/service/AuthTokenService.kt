f8be258d1e99590460783f65f061dea9c567ee64
Chanhyuk Yang <yangchanhk98@snu.ac.kr>
2023-05-15 14:45:30 +0900

    [FEAT] AuthTokenService의 getUsernameFromToken, parse 함수 & enum class(이름: Type) 구현

--- src/main/kotlin/com/wafflestudio/ggzz/domain/user/service/AuthTokenService.kt
+++ src/main/kotlin/com/wafflestudio/ggzz/domain/user/service/AuthTokenService.kt
@@ -1,9 +1,8 @@
 package com.wafflestudio.nostalgia.domain.user.service
 
-import com.wafflestudio.nostalgia.domain.user.dto.UserDto.*
-import io.jsonwebtoken.Claims
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import com.wafflestudio.nostalgia.global.error.InvalidTokenException
+import com.wafflestudio.nostalgia.global.error.TokenExpiredException
+import io.jsonwebtoken.*
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -22,7 +21,7 @@ class AuthTokenService (
     private val tokenPrefix = "Bearer "
     private val signingKey = Keys.hmacShaKeyFor(authProperties.jwtSecret.toByteArray())
 
-    fun generateAccessTokenByUsername(username: String): AuthToken {
+    fun generateAccessTokenByUsername(username: String): String {
         val claims: Claims = Jwts.claims()
         claims["username"] = username
         val issuer = authProperties.issuer
@@ -33,14 +32,13 @@ class AuthTokenService (
                 .atZone(ZoneId.systemDefault())
                 .toInstant()
         )
-        val resultToken = Jwts.builder()
+        return Jwts.builder()
             .setClaims(claims)
             .setIssuer(issuer)
             .setExpiration(expiryDate)
             .signWith(signingKey, SignatureAlgorithm.HS256)
             .compact()
 
-        return AuthToken(resultToken)
     }
 
     fun generateRefreshTokenByUsername(username: String): String {
@@ -62,6 +60,25 @@ class AuthTokenService (
             .compact()
     }
 
+    fun getUsernameFromToken(authToken: String, type: Type): String {
+        return try {
+            parse(authToken).body["username"] as String
+        } catch (e: ExpiredJwtException) {
+            throw TokenExpiredException(type.toString().lowercase())
+        } catch (e: Exception) {
+            throw InvalidTokenException(type.toString().lowercase())
+        }
+    }
+
+    private fun parse(authToken: String): Jws<Claims> {
+        val prefixRemoved = authToken.replace(tokenPrefix, "").trim { it <= ' ' }
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(signingKey)
+            .build()
+            .parseClaimsJws(prefixRemoved)
+    }
+
     fun generateResponseCookie(token: String): ResponseCookie {
         return ResponseCookie.from("refreshToken", token)
             .httpOnly(true)
@@ -71,6 +88,8 @@ class AuthTokenService (
             .maxAge(3600)
             .build()
     }
+}
 
-
+enum class Type{
+    ACCESS, REFRESH
 }
