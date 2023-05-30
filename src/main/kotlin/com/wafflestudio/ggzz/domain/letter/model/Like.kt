e5cfa674ce02022829a7a3ad3be3233e0a8ba965
Goniieee <gksrhs97@gmail.com>
2023-05-30 19:30:49 +0900

    [FIX] Like 엔티티 연관관계 수정

--- src/main/kotlin/com/wafflestudio/ggzz/domain/letter/model/Like.kt
+++ src/main/kotlin/com/wafflestudio/ggzz/domain/letter/model/Like.kt
@@ -3,14 +3,15 @@ package com.wafflestudio.ggzz.domain.letter.model
 import com.wafflestudio.ggzz.domain.user.model.User
 import com.wafflestudio.ggzz.global.common.model.BaseTimeTraceEntity
 import jakarta.persistence.Entity
-import jakarta.persistence.OneToOne
+import jakarta.persistence.FetchType
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "likes")
 class Like(
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     val user: User,
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     val letter: Letter,
 ): BaseTimeTraceEntity()
