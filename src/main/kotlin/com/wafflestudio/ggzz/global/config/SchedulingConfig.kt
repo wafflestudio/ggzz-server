66f6549fbb6ed6291764739cf5f8dbc7750fde68
Junhyeong Kim <indiv0227@snu.ac.kr>
2023-07-02 23:50:22 +0900

    [FIX] 구현 변경에 따라 SchedulingConfig 삭제

--- src/main/kotlin/com/wafflestudio/ggzz/global/config/SchedulingConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.wafflestudio.ggzz.global.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableScheduling
-
-@Configuration
-@EnableScheduling
-class SchedulingConfig
