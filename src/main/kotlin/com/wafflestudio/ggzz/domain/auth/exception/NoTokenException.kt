a1690a79d06343fce7c15fcc614b3551a1b346d7
Hangon Kim <gksrhs97@gmail.com>
2023-09-24 15:04:48 +0900

    [FIX] 빌드 시에 JAR파일에 docs 포함이 안되는 문제 해결 (#18)

--- src/main/kotlin/com/wafflestudio/ggzz/domain/auth/exception/NoTokenException.kt
+++ src/main/kotlin/com/wafflestudio/ggzz/domain/auth/exception/NoTokenException.kt
@@ -3,4 +3,4 @@ package com.wafflestudio.ggzz.domain.auth.exception
 import com.wafflestudio.ggzz.global.common.exception.CustomException
 import com.wafflestudio.ggzz.global.common.exception.ErrorType.Unauthorized.NO_TOKEN
 
-class NoTokenException: CustomException.UnauthorizedException(NO_TOKEN, "Token doesn't exist.")
+class NoTokenException: CustomException.UnauthorizedException(NO_TOKEN, "Token does not exist.")
